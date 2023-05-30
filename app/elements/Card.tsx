951c51177e8a7f7336dbd3417e98cfc52c7f9313
Panchavarnam Gopalakrishnan MTTS <gopalakrishnan.panchavarnam@mt.com>
2023-05-30 20:06:34 +0530

    Add local image to portfolio

--- app/elements/Card.tsx
+++ app/elements/Card.tsx
@@ -1,15 +1,19 @@
 import { FaCode, FaSearch } from "react-icons/fa";
+
+import Image from "next/image";
 import Button from "./Button";
 
 export default function Card(props: any) {
-    return <div className="rounded-lg overflow-hidden bg-white max-w-xs">
+    return <div className="rounded-lg overflow-hidden bg-white max-w-xs ">
         <div className="p-5 w-full">
             <h3 className="font-medium text-md py-2 text-sky-600 uppercase">{props.title}</h3>
             <p className="text-sm text-gray-500">
                 {props.description}
             </p>
         </div>
-        <img className="w-full px-5" src={props.src} alt="" />
+        <div className=" px-5">
+            <Image className="w-full p-3 border-2 border-gray-300" src={props.src} alt="" />
+        </div>
         <div className="flex align-items justify-center py-5 gap-2">
             <Button
                 bgcolor='bg-teal-600'
--- app/elements/Tile.tsx
+++ app/elements/Tile.tsx
@@ -3,7 +3,7 @@ import { FaStar } from "react-icons/fa";
 export default function Tile(props: any) {
     const ratings: JSX.Element[] = [];
     for (let i = 1; i <= props.rating; i++) {
-        ratings.push(<FaStar className="text-sm" key={i} />);
+        ratings.push(<FaStar className="text-sm text-white stroke stroke-red-700" key={i} />);
     }
     return <div className={`rounded-md p-2 mx-1 my-2 ${props.color} w-32 h-32 flex flex-col align-center justify-center text-xl border-2`}>
         <h3 className="text-white text-sm py-1 text-center text-white">{props.title}</h3>
