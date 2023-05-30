0c811c4d22280e8aac1ef90af440c7cdff8e28f7
gopalakrishnanpv <gopalakrishnan1505@gmail.com>
2023-05-30 08:32:18 +0530

    Update text color to white

--- app/elements/Tile.tsx
+++ app/elements/Tile.tsx
@@ -6,8 +6,8 @@ export default function Tile(props: any) {
         ratings.push(<FaStar className="text-sm" key={i} />);
     }
     return <div className={`rounded-md p-2 mx-1 my-2 ${props.color} w-32 h-32 flex flex-col align-center justify-center text-xl border-2`}>
-        <h3 className="text-white text-sm py-1 text-center text-gray-500">{props.title}</h3>
-        <div className="flex align-items justify-center w-full">
+        <h3 className="text-white text-sm py-1 text-center text-white">{props.title}</h3>
+        <div className="flex align-items justify-center w-full text-white">
             {ratings}
         </div>
     </div>
