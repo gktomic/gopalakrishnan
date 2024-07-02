a845911d2c51a5d391bf36dcec319e458da99a6f
gkcodez <gopalakrishnan.work@gmail.com>
2024-07-02 21:04:02 +0530

    Resume, Achievements and About section updated

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -6,28 +6,28 @@ export function About() {
         <FaTerminal className="text-8xl text-white w-full" />
         <div className="flex flex-col items-center justify-center pt-5">
             <h1 className="text-4xl text-white pr-3 font-medium">I&apos;m Gopalakrishnan.<span className="uppercase animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-3 text-4xl text-white leading-tight"></span></h1>
-            <h3 className="text-md mt-2 text-gray-500">Software Engineer Professional - In Test</h3>
+            <h3 className="text-md mt-2 text-gray-500">Software Engineer Senior - In Test</h3>
             <h3 className="text-md mt-2 text-gray-500">Coimbatore</h3>
         </div>
 
         <div className="mt-5">
-            <p className="text-md text-gray-100">I have 8+ years of industry experience which includes automation testing in Web, Mobile and Desktop applications. </p>
+            <p className="text-md text-gray-100">I have 9+ years of industry experience which includes automation testing in Web, Mobile and Desktop applications. </p>
             <p className="mt-3 text-md text-gray-100">During my free time, I work on developing web applications using technologies like Angular, React, Tailwind and Bootstrap.</p>
         </div>
         <div className="w-full flex flex-wrap align-items justify-center pt-10 gap-3">
-            <a href="https://www.linkedin.com/in/gopalakrishnanpv/" target={"_blank"}>
+            <a href="https://www.linkedin.com/in/gktalkz" target={"_blank"}>
                 <Button
                     bgcolor='bg-sky-700'
                     icon={<FaLinkedin />}
                     text={'Linked In'} />
             </a>
-            <a href="https://github.com/gopalakrishnanpv" target={"_blank"}>
+            <a href="https://github.com/gkcodez" target={"_blank"}>
                 <Button
                     bgcolor='bg-slate-700'
                     icon={<FaGithub />}
                     text={'GitHub'} />
             </a>
-            <a href="mailto:gopalakrishnan1505@gmail.com" target={"_blank"}>
+            <a href="mailto:gopalakrishnan.work@gmail.com" target={"_blank"}>
                 <Button
                     bgcolor='bg-pink-600'
                     icon={<FaMailBulk />}
--- app/components/Achievements.tsx
+++ app/components/Achievements.tsx
@@ -5,6 +5,8 @@ export function Achievements() {
     return <div id="achievementsSection">
         <h1 className="mt-10 text-3xl font-medium uppercase text-white text-center underline underline-offset-[16px] decoration-4 decoration-dotted decoration-teal-500">Achievements</h1>
         <ul className="mt-5 text-white">
+            <Achievement description="Developed TAFManager, a full stack application in Vue 3 and python for test design." />
+            <Achievement description="Received Well Done Champ award for concurrent testing implementation." />
             <Achievement description="Developed full stack Employee Portal application for managing employee records." />
             <Achievement description="Won First Prize in IVS Testathon conducted at the unit level." />
             <Achievement description="Received Finastra Star Award in the Take Ownership Category." />
@@ -15,13 +17,5 @@ export function Achievements() {
             <Achievement description="Developed Research BOT for automating data point research activities." />
             <Achievement description="Developed BuildBOT for automating daily build updates." />
         </ul>
-
-
-
-
-
-
-
-
     </div>
 }
